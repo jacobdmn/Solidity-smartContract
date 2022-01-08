f13b2f08102fa892b08bf95705c743af764f6547
Jacob GI <jacob.demmane@gmail.com>
2022-01-08 17:57:26 +0300

    update test func

--- compile.js
+++ compile.js
@@ -5,4 +5,4 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
-module.exports = console.log(solc.compile(source, 1)).contracts[":Inbox"];
+module.exports = console.log(solc.compile(source, 1).contracts[":Inbox"]);
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,21 @@
 const assert = require("assert");
-const ganache = require("ganache");
+const ganache = require("ganache-cli");
 const Web3 = require("web3");
 
 const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+  try {
+    accounts = await web3.eth.getAccounts();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
+describe("Inbox", () => {
+  it("deployed the contract", () => {
+    console.log(accounts);
+  });
+});
